5330f1ec99f7026b72ad85fc001e7732396fc23f
sara.blom <sara.blom@medtankengroup.se>
2023-09-14 15:58:41 +0200

    add animation to divison group demo

--- src/components/DivisionGroupsDemo/DivisionGroupsDemo.js
+++ src/components/DivisionGroupsDemo/DivisionGroupsDemo.js
@@ -1,30 +1,21 @@
-'use client';
-import React from 'react';
-import clsx from 'clsx';
+"use client";
+import React from "react";
+import clsx from "clsx";
+import { motion } from "framer-motion";
 
-import { range } from '@/utils';
-import Card from '@/components/Card';
-import SliderControl from '@/components/SliderControl';
+import { range } from "@/utils";
+import Card from "@/components/Card";
+import SliderControl from "@/components/SliderControl";
 
-import Equation from './Equation';
-import styles from './DivisionGroupsDemo.module.css';
+import Equation from "./Equation";
+import styles from "./DivisionGroupsDemo.module.css";
 
-function DivisionGroupsDemo({
-  numOfItems = 12,
-  initialNumOfGroups = 1,
-  includeRemainderArea,
-}) {
-  const [numOfGroups, setNumOfGroups] = React.useState(
-    initialNumOfGroups
-  );
+function DivisionGroupsDemo({ numOfItems = 12, initialNumOfGroups = 1, includeRemainderArea }) {
+  const [numOfGroups, setNumOfGroups] = React.useState(initialNumOfGroups);
 
-  const numOfItemsPerGroup = Math.floor(
-    numOfItems / numOfGroups
-  );
+  const numOfItemsPerGroup = Math.floor(numOfItems / numOfGroups);
 
-  const remainder = includeRemainderArea
-    ? numOfItems % numOfGroups
-    : null;
+  const remainder = includeRemainderArea ? numOfItems % numOfGroups : null;
 
   // When we're splitting into 1-3 groups, display side-by-side
   // columns. When we get to 4, it should switch to a 2x2 grid.
@@ -34,8 +25,8 @@ function DivisionGroupsDemo({
           gridTemplateColumns: `repeat(${numOfGroups}, 1fr)`,
         }
       : {
-          gridTemplateColumns: '1fr 1fr',
-          gridTemplateRows: '1fr 1fr',
+          gridTemplateColumns: "1fr 1fr",
+          gridTemplateRows: "1fr 1fr",
         };
 
   return (
@@ -48,24 +39,21 @@ function DivisionGroupsDemo({
           min={1}
           max={4}
           value={numOfGroups}
-          onChange={(ev) =>
-            setNumOfGroups(Number(ev.target.value))
-          }
+          onChange={(ev) => setNumOfGroups(Number(ev.target.value))}
         />
       </header>
 
       <div className={styles.demoWrapper}>
-        <div
-          className={clsx(styles.demoArea)}
-          style={gridStructure}
-        >
+        <div className={clsx(styles.demoArea)} style={gridStructure}>
           {range(numOfGroups).map((groupIndex) => (
             <div key={groupIndex} className={styles.group}>
               {range(numOfItemsPerGroup).map((index) => {
                 return (
-                  <div
+                  <motion.div
                     key={index}
                     className={styles.item}
+                    layout={true}
+                    transition={{ type: "spring", stiffness: 300, damping: 40 }}
                   />
                 );
               })}
@@ -76,23 +64,15 @@ function DivisionGroupsDemo({
 
       {includeRemainderArea && (
         <div className={styles.remainderArea}>
-          <p className={styles.remainderHeading}>
-            Remainder Area
-          </p>
+          <p className={styles.remainderHeading}>Remainder Area</p>
 
           {range(remainder).map((index) => {
-            return (
-              <div key={index} className={styles.item} />
-            );
+            return <div key={index} className={styles.item} />;
           })}
         </div>
       )}
 
-      <Equation
-        dividend={numOfItems}
-        divisor={numOfGroups}
-        remainder={remainder}
-      />
+      <Equation dividend={numOfItems} divisor={numOfGroups} remainder={remainder} />
     </Card>
   );
 }
