0b3ae9df73e90201b46579806ccf569193efe2ee
Sara Blom <sara.cecilia.blom@gmail.com>
2023-09-23 12:24:39 +0200

    formatting

--- src/app/page.js
+++ src/app/page.js
@@ -7,28 +7,28 @@ import { BLOG_TITLE } from "@/constants";
 import styles from "./homepage.module.css";
 
 export const metadata = {
-    title: BLOG_TITLE,
-		content: "A wonderful blog about JavaScript"
+  title: BLOG_TITLE,
+  content: "A wonderful blog about JavaScript",
 };
 
 async function Home() {
-    const blogPosts = await getBlogPostList();
+  const blogPosts = await getBlogPostList();
 
-    return (
-        <div className={styles.wrapper}>
-            <h1 className={styles.mainHeading}>Latest Content:</h1>
-            {blogPosts.length > 0 &&
-                blogPosts.map(({ title, slug, abstract, publishedOn }) => (
-                    <BlogSummaryCard
-                        key={slug}
-                        title={title}
-                        slug={slug}
-                        abstract={abstract}
-                        publishedOn={publishedOn}
-                    />
-                ))}
-        </div>
-    );
+  return (
+    <div className={styles.wrapper}>
+      <h1 className={styles.mainHeading}>Latest Content:</h1>
+      {blogPosts.length > 0 &&
+        blogPosts.map(({ title, slug, abstract, publishedOn }) => (
+          <BlogSummaryCard
+            key={slug}
+            title={title}
+            slug={slug}
+            abstract={abstract}
+            publishedOn={publishedOn}
+          />
+        ))}
+    </div>
+  );
 }
 
 export default Home;
