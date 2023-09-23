a7ea020c6b0c0259c6267431c68caa2a89376103
Sara Blom <sara.cecilia.blom@gmail.com>
2023-09-23 12:02:37 +0200

    add dark / light mode

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,36 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import clsx from "clsx";
 import { Rss, Sun, Moon } from "react-feather";
+import Cookie from "js-cookie";
 
 import Logo from "@/components/Logo";
 import VisuallyHidden from "@/components/VisuallyHidden";
 
+import { LIGHT_TOKENS, DARK_TOKENS } from "@/constants";
 import styles from "./Header.module.css";
 
-function Header({ theme, className, ...delegated }) {
+function Header({ startTheme, className, ...delegated }) {
+  const [theme, setTheme] = useState(startTheme);
+
+  function handleClick() {
+    const nextTheme = theme === "light" ? "dark" : "light";
+
+    setTheme(nextTheme);
+
+    Cookie.set("color-theme", nextTheme, {
+      expires: 1000,
+    });
+
+    const root = document.documentElement;
+    const colors = nextTheme === "light" ? LIGHT_TOKENS : DARK_TOKENS;
+
+    root.setAttribute("data-color-theme", nextTheme);
+    Object.entries(colors).forEach(([key, value]) => {
+      root.style.setProperty(key, value);
+    });
+  }
+
   return (
     <header className={clsx(styles.wrapper, className)} {...delegated}>
       <Logo />
@@ -23,7 +46,7 @@ function Header({ theme, className, ...delegated }) {
           />
           <VisuallyHidden>View RSS feed</VisuallyHidden>
         </button>
-        <button className={styles.action}>
+        <button className={styles.action} onClick={handleClick}>
           {theme === "light" ? <Sun size="1.5rem" /> : <Moon size="1.5rem" />}
           <VisuallyHidden>Toggle dark / light mode</VisuallyHidden>
         </button>
